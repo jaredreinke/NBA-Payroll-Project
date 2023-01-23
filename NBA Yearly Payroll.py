26da782f8da2c76f66ae538fc44924547e031c0d
jreinke11223344 <jaredreinke@gmail.com>
2023-01-22 20:21:21 -0500

    Add files via upload

---
+++ NBA Yearly Payroll.py
@@ -0,0 +1,78 @@
+# Import neccesary modules
+import requests
+import os
+import shutil
+from bs4 import BeautifulSoup
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Create a list of seasons to scrape
+years = list(range(1991, 2024))
+
+seasons = []
+for year in years:
+    start_year = str(year - 1)
+    end_year = str(year)
+    season = start_year + "-" + end_year
+    seasons.append(season)
+
+
+print(seasons)
+
+# Get html text for team salary and save to salaries folder for faster loading
+
+base_url = "https://hoopshype.com/salaries/{}"
+
+for season in seasons:
+    url = base_url.format(season)
+    response = requests.get(url)
+    with open("Salaries/{}".format(season), "w+") as S:
+        S.write(response.text)
+
+# Read the HTML files and create a dataframe
+dfs = []
+dfs
+
+for season in seasons:
+    with open("Salaries/{}".format(season)) as S:
+        page = S.read()
+    soup = BeautifulSoup(page, "html.parser")
+    table = soup.find(
+        "table",
+        class_="hh-salaries-ranking-table hh-salaries-table-sortable responsive",
+    )
+    salaries = pd.read_html(str(table))[0]
+    salaries["Season"] = season
+    salaries.columns.values[0] = "Rank"
+    salaries.columns.values[2] = "Yearly_Payroll"
+    salaries.columns.values[-2] = "Yearly_Payroll_2023_Dollars"
+    dfs.append(salaries)
+
+salaries = pd.concat(dfs)
+salaries.tail()
+
+# Extract to CSV to double check data
+
+salaries.to_csv("salaries.csv")
+
+# Load to Postgres Database
+
+import psycopg2
+
+# Upload to SQL Database
+conn = psycopg2.connect(
+    host="localhost", port=5432, database="NBA", user="postgres", password="postgres"
+)
+
+conn.autocommit = True
+
+cur = conn.cursor()
+conn.rollback()
+
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/NBA")
+
+salaries.to_sql("Salaries", engine, if_exists="replace")
+
+conn.commit()
+
+conn.close()
---
+++ Year by Year Stats NBA API.py
@@ -0,0 +1,48 @@
+from nba_api.stats.endpoints import teamyearbyyearstats
+from nba_api.stats.endpoints import leaguestandingsv3
+from nba_api.stats.library.parameters import SeasonAll
+from nba_api.stats.static import teams
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+import time
+
+teams_list = teams.get_teams()
+teams_list = pd.DataFrame(teams_list)
+id_list = teams_list["id"].to_list()
+
+years = list(range(1990, 2024))
+
+seasons = []
+for year in years:
+    start_year = str(year - 1)
+    end_year = str(year)
+    end_year_short = end_year[-2:]
+    season = start_year + "-" + end_year_short
+    seasons.append(season)
+
+year_over_year = pd.DataFrame()
+for id in id_list:
+    year_by_year = teamyearbyyearstats.TeamYearByYearStats(league_id="00", team_id=id)
+
+    df_year_by_year = pd.DataFrame(year_by_year.get_data_frames()[0])
+    year_over_year = pd.concat([year_over_year, df_year_by_year])
+    time.sleep(5)
+
+# Upload to SQL Database
+conn = psycopg2.connect(
+    host="localhost", port=5432, database="NBA", user="postgres", password="postgres"
+)
+
+conn.autocommit = True
+
+cur = conn.cursor()
+conn.rollback()
+
+engine = create_engine("postgresql://postgres:postgres@localhost:5432/NBA")
+
+year_over_year.to_sql("Year Over Year", engine, if_exists="replace")
+
+conn.commit()
+
+conn.close()
